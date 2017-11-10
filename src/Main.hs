e1aed87a40720922f4a748fac2aee6ce5830ce0e
Michael Xu <michaeljxu11@gmail.com>
2017-11-09 23:15:45 -0800

    some testing directory walking code

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
-module Main where
+import Data.Traversable      (traverse)
+import System.IO             (stdin, stdout)
+import System.Environment
+import System.Exit
+import System.Directory.Tree (AnchoredDirTree(..), DirTree(..), filterDir, readDirectoryWith)
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = getArgs >>= parse
+
+parse ["-h"]   = usage   >> exit
+parse ["-v"]   = version >> exit
+parse []       = usage >> exit
+
+parse path = do
+    _:/tree <- readDirectoryWith return (head path)
+    traverse putStrLn $ filterDir myPred tree
+    return ()
+  where myPred (Dir ('.':_) _) = False
+        myPred _ = True
+
+usage   = putStrLn "Usage: directory-watch [path]"
+version = putStrLn "directory-watch 0.1"
+exit    = exitWith ExitSuccess
+die     = exitWith (ExitFailure 1)
